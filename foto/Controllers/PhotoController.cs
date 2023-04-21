4107dcff006be8d2695c4a7fe45955a7fed3209d
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-04-21 16:55:22 +0200

    creazione crud da aggiungere il booleano visible

--- foto/Controllers/PhotoController.cs
+++ foto/Controllers/PhotoController.cs
@@ -33,5 +33,85 @@ namespace foto.Controllers
             return View(singleuser);
             ;
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Photo data)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Create",data);
+            }
+            Photo newphoto = new Photo();
+            newphoto.Title = data.Title;
+            newphoto.Description = data.Description;
+            newphoto.ImageUrl = data.ImageUrl;
+            _context.Photos.Add(newphoto);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Photo editphoto = _context.Photos.Where(photo => photo.Id == id).FirstOrDefault();
+            if(editphoto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(editphoto);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Photo data)
+        {
+           /* if(ModelState.IsValid)
+            {
+                return View("Edit", data);
+            }*/
+            Photo newphotoedit = _context.Photos.Where(p=> p.Id == id).FirstOrDefault();
+
+            if(newphotoedit != null)
+            {
+                newphotoedit.Title= data.Title;
+                newphotoedit.Description= data.Description;
+                newphotoedit.ImageUrl = data.ImageUrl;
+                _context.SaveChanges();
+                return RedirectToAction("Index", data);
+
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            Photo phototodelete =_context.Photos.Where(m=>m.Id==id).FirstOrDefault();
+
+            if(phototodelete != null)
+            {
+                _context.Photos.Remove(phototodelete);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        
     }
 }
